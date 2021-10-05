0c19463fde517e62907f05466eb4d40e037aa1c0
Jacob Ressler <jressler96@gmail.com>
2021-10-05 14:37:57 -0400

    set document title on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent  implements OnInit {
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.setTitle('Portfolio | Jacob Ressler');
+  }
+
+  setTitle(str: string) {
+    this.titleService.setTitle(str);
+  }
 }
